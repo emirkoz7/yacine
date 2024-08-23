bab4cab0a41ceb5ecd0d1088db2ef3d9cc2019a2
emirkoz7 <dani7barca@gmail.com>
2024-08-23 21:08:18 +0100

    Update config.js

--- site/src/scripts/config.js
+++ site/src/scripts/config.js
@@ -195,7 +195,7 @@ export const proxy = {
 export const proxies =
   process?.env?.NODE_ENV === "development"
     ? ["http://localhost:2000/api"]
-    : ["https://film-haven.vercel.app/api"];
+    : ["https://yacine-sigma.vercel.app/api"];
 
 export const store = {
   names: {
